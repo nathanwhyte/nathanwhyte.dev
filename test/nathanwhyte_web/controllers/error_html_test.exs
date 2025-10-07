f8616edbc7070e86ab57c6777070b499ba406eca
Nathan Whyte <nathanwhyte35@gmail.com>
2025-10-07 15:22:37 -0500

    Install and run `pre-commit` hooks

--- test/nathanwhyte_web/controllers/error_html_test.exs
+++ test/nathanwhyte_web/controllers/error_html_test.exs
@@ -9,6 +9,7 @@ defmodule NathanwhyteWeb.ErrorHTMLTest do
   end
 
   test "renders 500.html" do
-    assert render_to_string(NathanwhyteWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+    assert render_to_string(NathanwhyteWeb.ErrorHTML, "500", "html", []) ==
+             "Internal Server Error"
   end
 end
