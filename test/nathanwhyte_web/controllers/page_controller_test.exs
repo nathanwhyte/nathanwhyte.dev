9b51e06667928d444513052edb77cff17a4a3606
Nathan Whyte <nathanwhyte35@gmail.com>
2025-10-08 22:08:54 -0500

    Add some very basic tests for navbar and home page

--- test/nathanwhyte_web/controllers/page_controller_test.exs
+++ test/nathanwhyte_web/controllers/page_controller_test.exs
@@ -3,6 +3,12 @@ defmodule NathanwhyteWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Nathan Whyte"
+    html = html_response(conn, 200)
+
+    # Make sure navbar is present
+    assert html =~ ~r/<header[^>]*class="navbar/
+    assert html =~ "Nathan Whyte"
+
+    # TODO: update tests when more home page content is added
   end
 end
