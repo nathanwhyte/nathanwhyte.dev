d615a6bfaaedebc69adf8012955f011f1e58a442
Nathan Whyte <nathanwhyte35@gmail.com>
2025-10-11 21:33:28 -0500

    Make sure default greeting value isn't empty

--- lib/nathanwhyte_web/live/home_page_live.ex
+++ lib/nathanwhyte_web/live/home_page_live.ex
@@ -2,7 +2,7 @@ defmodule NathanwhyteWeb.HomePageLive do
   use NathanwhyteWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, :greeting, "")}
+    {:ok, assign(socket, :greeting, "Hello!")}
   end
 
   def handle_event("report_user_time", params, socket) do
@@ -20,10 +20,10 @@ defmodule NathanwhyteWeb.HomePageLive do
 
     greeting =
       case local_date_time.hour do
-        h when h in 0..11 -> "Good morning"
-        h when h in 12..17 -> "Good afternoon"
-        h when h in 18..23 -> "Good evening"
-        _ -> "Hello"
+        h when h in 0..11 -> "Good morning!"
+        h when h in 12..16 -> "Good afternoon!"
+        h when h in 17..23 -> "Good evening!"
+        _ -> "Hello!"
       end
 
     {:noreply, assign(socket, :greeting, greeting)}
