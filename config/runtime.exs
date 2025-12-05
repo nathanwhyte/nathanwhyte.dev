6cac064b76f3eb5143bba64ae545e1d875fb56d3
Nathan Whyte <nathanwhyte35@gmail.com>
2025-12-04 18:55:50 -0600

    Deploy to portfolio.nathanwhyte.dev using Kubernetes

--- config/runtime.exs
+++ config/runtime.exs
@@ -57,7 +57,7 @@ if config_env() == :prod do
 
   config :nathanwhyte, NathanwhyteWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
-    check_origin: ["//#{host}", "//www.#{host}"],
+    check_origin: ["//#{host}", "//www.#{host}", "//portfolio.#{host}"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
