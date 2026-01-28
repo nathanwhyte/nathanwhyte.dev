2bf0004dc45524de78e8b165046d56ff497e1567
Nathan Whyte <nathanwhyte35@gmail.com>
2026-01-28 13:44:04 -0600

    Fix tailwind_formatter setup and rules

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,5 @@
 [
   import_deps: [:phoenix],
   plugins: [TailwindFormatter, Phoenix.LiveView.HTMLFormatter],
-  inputs: ["*.{heex,ex,exs}", "{config,lib,test}/**/*.{heex,ex,exs}"]
+  inputs: ["*.{heex,ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{heex,ex,exs}"]
 ]
